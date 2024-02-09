f59e6b3b63cb4f8e1a90f28b2a0f16c354033abd
krixtomss <152053127+krixtomss@users.noreply.github.com>
2024-02-09 16:50:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['917605902011','919365354223'] 
+global.prems = ['917605902011', '32460248586', '919365354223']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '_/𝑳𝒀𝑮𝑶𝑷𝑯𝑰𝑳𝑬ت︎'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'KRIXTOM' 
+global.author = '_/𝑳𝒀𝑮𝑶𝑷𝑯𝑰𝑳𝑬ت︎' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
